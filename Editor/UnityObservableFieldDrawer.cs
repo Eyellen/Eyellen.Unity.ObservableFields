a3032487d5a83000eeb8ce614848f2e6e930aeb2
Eyellen <83500523+Eyellen@users.noreply.github.com>
2024-10-15 12:24:41 +0300

    code clean up

--- Editor/UnityObservableFieldDrawer.cs
+++ Editor/UnityObservableFieldDrawer.cs
@@ -68,29 +68,31 @@ namespace Eyellen.Unity.ObservableFields.Editor
 
             if (hasBeenChanged)
             {
-                BindingFlags flags =
-                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-
-                UnityEngine.Object targetObject = property.serializedObject.targetObject;
-
-                object propertyValue = targetObject
-                    .GetType()
-                    .GetField(property.name, flags)
-                    .GetValue(targetObject);
-
-                Type propertyType = propertyValue.GetType();
-
-                object value = propertyType
-                    .GetField(m_SerializedValuePath, flags)
-                    .GetValue(propertyValue);
-
-                MethodInfo setMethod = propertyType
-                    .BaseType.GetProperty("Value", flags)
-                    .GetSetMethod();
-                setMethod.Invoke(propertyValue, new[] { value });
-
+                AssignValue(property);
                 property.serializedObject.ApplyModifiedProperties();
             }
+        }
+
+        private void AssignValue(SerializedProperty property)
+        {
+            BindingFlags flags =
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+            UnityEngine.Object targetObject = property.serializedObject.targetObject;
+
+            object propertyValue = targetObject
+                .GetType()
+                .GetField(property.name, flags)
+                .GetValue(targetObject);
+
+            Type propertyType = propertyValue.GetType();
+
+            object value = propertyType
+                .GetField(m_SerializedValuePath, flags)
+                .GetValue(propertyValue);
+
+            MethodInfo setMethod = propertyType.BaseType.GetProperty("Value", flags).GetSetMethod();
+            setMethod.Invoke(propertyValue, new[] { value });
         }
     }
 }
