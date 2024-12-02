41c7140bf4346f7c2b3f0eced22ceb799d3ca384
Eyellen <83500523+Eyellen@users.noreply.github.com>
2024-12-02 03:17:08 +0300

    add IObservableFieldEvents interface

---
+++ Runtime/IObservableFieldEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Eyellen.Unity.ObservableFields
+{
+    /// <summary>
+    /// This interface provides methods to subscribe or unsubscribe from changes.
+    /// </summary>
+    public interface IObservableFieldEvents<T>
+    {
+        public void SubscribeOnChange(Action<ObservableField<T>.EventArgs> callback);
+
+        public void SubscribeOnChange(Action<T, T> callback);
+
+        public void SubscribeOnChange(Action callback);
+
+        public void UnsubscribeOnChange(Action<ObservableField<T>.EventArgs> callback);
+
+        public void UnsubscribeOnChange(Action<T, T> callback);
+
+        public void UnsubscribeOnChange(Action callback);
+    }
+}
--- Runtime/IReadOnlyObservableField.cs
+++ Runtime/IReadOnlyObservableField.cs
@@ -1,24 +1,10 @@
-﻿using System;
-
-namespace Eyellen.Unity.ObservableFields
+﻿namespace Eyellen.Unity.ObservableFields
 {
     /// <summary>
     /// Interface to access read only value and methods to subscribe on changes.
     /// </summary>
-    public interface IReadOnlyObservableField<T>
+    public interface IReadOnlyObservableField<T> : IObservableFieldEvents<T>
     {
         public T Value { get; }
-
-        public void SubscribeOnChange(Action<ObservableField<T>.EventArgs> callback);
-
-        public void SubscribeOnChange(Action<T, T> callback);
-
-        public void SubscribeOnChange(Action callback);
-
-        public void UnsubscribeOnChange(Action<ObservableField<T>.EventArgs> callback);
-
-        public void UnsubscribeOnChange(Action<T, T> callback);
-
-        public void UnsubscribeOnChange(Action callback);
     }
 }
